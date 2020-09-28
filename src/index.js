9584f7dcee78be95e65f050426363868c57db2cd
Alexey Cherkas <alexey.cherkas1997@gmail.com>
2020-09-28 23:55:09 +0300

    feat: implement min function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+exports.min = function min(array) {
+    if (Array.isArray(array)) {
+        if (array.length > 0) {
+            let min = array[0];
+            for (let i = 0; i < array.length; i++) {
+                if (array[i] < min) {
+                    min = array[i];
+                }
+            }
+        return min;
+        }
+        else {
+            return 0;
+        }
+    }    
+    else {
+        return 0;
+    }
+}
+
+
 
